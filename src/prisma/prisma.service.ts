fcf86a522415a9357d72e7bd846e50faed620783
Pedro Ferreira <pedrolcsferreira@gmail.com>
2024-01-28 14:16:47 -0300

    configure prisma provider

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  public client: PrismaClient
+
+  constructor() {
+    super({
+      log: ['warn', 'error'],
+    })
+  }
+
+  // make sure to call `$conect` on init
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  // make sure to call `$disconnect` on destroy
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
